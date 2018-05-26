d89415ecf71697fa8ddf7227e3b2a390fb65ef57
Clay Rehling <clayrehling@hotmail.com>
2018-05-26 16:10:09 -0500

    assets
    assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D myRigidbody;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // from -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+}
