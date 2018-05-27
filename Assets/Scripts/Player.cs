8dc64bab126137e0d2ae00f2f6363ca6c7db964c
Clay Rehling <clayrehling@hotmail.com>
2018-05-27 18:48:02 -0500

    rolled my own layer detection - works
    rolled my own layer detection - works, now to compare to Rick's

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,12 +15,14 @@ public class Player : MonoBehaviour {
     // Cached component references
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    Collider2D myCollider2D;
 
     // Message then methods (?)
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
     private void Update()
     {
@@ -34,14 +36,17 @@ public class Player : MonoBehaviour {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // from -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
-        print(playerVelocity);
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
     }
 
     private void Jump()
     {
-        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        int layerToCheckForCollision = LayerMask.GetMask("Ground");
+        bool notTouchingGround = (myCollider2D.IsTouchingLayers(layerToCheckForCollision));
+        bool jumpButtonDown = (CrossPlatformInputManager.GetButtonDown("Jump"));
+
+        if ( notTouchingGround && jumpButtonDown)
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidbody.velocity += jumpVelocityToAdd;
