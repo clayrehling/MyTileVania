b02cf98e15e858fed38f580cbb03c3697cdad908
Clay Rehling <clayrehling@hotmail.com>
2018-05-27 17:17:50 -0500

    #330 finished - run animation
    #330 finished - run animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,36 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+    // Config
     [SerializeField] float runSpeed = 5f;
-    Rigidbody2D myRigidbody;
 
+    // State
+    bool isAlive = true;
+
+    // Cached component references
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+    // Message then methods (?)
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
     private void Update()
     {
         Run();
         FlipSprite();
-    }
+    } 
 
     private void Run()
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // from -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
     }
 
     private void FlipSprite()
