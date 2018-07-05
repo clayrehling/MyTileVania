40c21d634ae1f7b84aabcec1437082ac5d59fb74
Unknown <clayrehling@hotmail.com>
2018-07-05 16:56:23 -0500

    runout

---
+++ Assets/LevelExit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D()
+    {
+        // StartCoroutine 
+    }
+
+    IEnumerator HasAName
+    {
+        // yeild with a delay
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
